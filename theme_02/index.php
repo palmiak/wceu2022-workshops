87ad3816827ba35d08f9ea094a812bfd5b49d173
Maciej Palmowski <m.palmowski@freshpixels.pl>
2022-06-03 22:45:40 +0100

    timber fix

--- theme_02/index.php
+++ theme_02/index.php
@@ -8,4 +8,4 @@
 $context          = Timber::get_context();
 $context['posts'] = Timber::get_posts();
 
-Timber::render( 'views/templates/index.twig', $context );
+Timber::render( 'views/index.twig', $context );
